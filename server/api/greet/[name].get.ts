bc2d50b5d67b998b6f6c1ac2e1e432ba1c197f86
HuangKai <mail.huangkai@gmail.com>
2023-04-22 11:57:30 +0800

    修复语法存在错误提示的问题

--- server/api/greet/[name].get.ts
+++ server/api/greet/[name].get.ts
@@ -1,7 +1,11 @@
 export default defineEventHandler(({ context }) => {
-  const {
-    params: { name },
-  } = context;
+  // Property 'name' does not exist on type 'Record<string, string> | undefined'.
+  // const {
+  //   params: { name },
+  // } = context;
+
+  // const { params } = context;
+  // const name = params?.name;
 
   return {
     message: `你好 ${name}，欢迎来宁皓网学习 Nuxt`,
