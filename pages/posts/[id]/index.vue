b8bfefee5bcb441a9aba0a174c5a0705e09ea32f
HuangKai <mail.huangkai@gmail.com>
2023-04-26 20:58:03 +0800

    nuxtjs-w22-05-01-08-内容页面

--- pages/posts/[id]/index.vue
+++ pages/posts/[id]/index.vue
@@ -1,11 +1,31 @@
 <template>
   <div>
-    <h1>内容页面 {{ id }}</h1>
+    <Head>
+      <Title>{{ post?.title }}</Title>
+    </Head>
   </div>
+  <img
+    v-if="post?.file"
+    style="100%"
+    :src="`${apiBaseUrl}/files/${post?.file?.id}/serve?size=large`"
+    alt=""
+  />
+  <div v-else>没有图片</div>
+  <h1>{{ post?.title }}</h1>
+  <div>{{ post?.content }}</div>
+  <div>{{ post?.user.name }}</div>
 </template>
 
 <script setup lang="ts">
+import { Post } from '~/types/post.type';
+
 const {
   params: { id },
 } = useRoute();
+
+const {
+  public: { apiBaseUrl },
+} = useRuntimeConfig();
+
+const { data: post } = await useApiFetch<Post>(`posts/${id}`);
 </script>
